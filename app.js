43e2ab2ee003561c0330bc64186eb7d427b9b21c
Jeff Juell <87201199+jefejuell@users.noreply.github.com>
2022-01-24 21:23:47 -0600

    Users can see all reviews

--- app.js
+++ app.js
@@ -6,15 +6,27 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
-
 app.get('/', (req, res) => {
     //res.send('Hello World!');
-    res.render('home', { msg: 'Handlebars are Cool!' });
+    //res.render('home', { msg: 'Handlebars are Cool!' });
+    res.render('reviews-index', { reviews: reviews });
 })
 
-app.get('/about', (req, res) => {
-    res.send("This is a movie review website");
-})
+// app.get('/about', (req, res) => {
+//     res.send("This is a movie review website");
+// })
+
 app.listen(3000, () => {
     console.log('Server is listening on port 3000')
 })
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+  ]
+  
+  // INDEX
+//   app.get('/reviews', (req, res) => {
+//     res.render('reviews-index', { reviews: reviews });
+//   })
