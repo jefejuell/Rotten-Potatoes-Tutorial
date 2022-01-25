ae8157532c92370bd6e5c41dc59300ca50ee0c3c
Jeff Juell <87201199+jefejuell@users.noreply.github.com>
2022-01-25 09:02:57 -0600

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
 //https://stackoverflow.com/questions/69959820/typeerror-exphbs-is-not-a-function
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+  });
 
 const app = express();
 
@@ -8,11 +15,11 @@ app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
-app.get('/', (req, res) => {
-    //res.send('Hello World!');
-    //res.render('home', { msg: 'Handlebars are Cool!' });
-    res.render('reviews-index', { reviews: reviews });
-})
+// app.get('/', (req, res) => {
+//     //res.send('Hello World!');
+//     //res.render('home', { msg: 'Handlebars are Cool!' });
+//     res.render('reviews-index', { reviews: reviews });
+// })
 
 // app.get('/about', (req, res) => {
 //     res.send("This is a movie review website");
@@ -23,13 +30,20 @@ app.listen(3000, () => {
 })
 
 // OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-    { title: "Great Review", movieTitle: "Batman II" },
-    { title: "Awesome Movie", movieTitle: "Titanic" },
-    { title: "Pensive Thriller", movieTitle: "Shawshank Redemption"}
-  ]
+// let reviews = [
+//     { title: "Great Review", movieTitle: "Batman II" },
+//     { title: "Awesome Movie", movieTitle: "Titanic" },
+//     { title: "Pensive Thriller", movieTitle: "Shawshank Redemption"}
+//   ]
   
   // INDEX
-//   app.get('/reviews', (req, res) => {
-//     res.render('reviews-index', { reviews: reviews });
-//   })
+  app.get('/', (req, res) => {
+    Review.find()
+        .then(reviews => {
+           res.render('reviews-index', { reviews: reviews }); 
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    
+  })
